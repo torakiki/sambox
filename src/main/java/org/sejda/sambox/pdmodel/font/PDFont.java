09ade5c0a3607bb01d97c1769bfefddfe35ca26e
Andrea Vacondio <andrea.vacondio@gmail.com>
2016-02-19 11:55:36 +0100

    Added missing required type

--- src/main/java/org/sejda/sambox/pdmodel/font/PDFont.java
+++ src/main/java/org/sejda/sambox/pdmodel/font/PDFont.java
@@ -78,6 +78,7 @@ public abstract class PDFont implements COSObjectable, PDFontLike
     PDFont(String baseFont)
     {
         dict = new COSDictionary();
+        dict.setItem(COSName.TYPE, COSName.FONT);
         toUnicodeCMap = null;
         afmStandard14 = Standard14Fonts.getAFM(baseFont);
         if (afmStandard14 == null)
