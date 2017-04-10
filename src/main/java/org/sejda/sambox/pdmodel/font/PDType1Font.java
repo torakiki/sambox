c9ac8e6dd13adddead228d4c1da358ce1a6b7a71
Edi Weissmann <edi.weissmann@gmail.com>
2017-04-10 11:07:26 +0200

    Throws a specific exception when attempting to use a unsupported font and character combination

--- src/main/java/org/sejda/sambox/pdmodel/font/PDType1Font.java
+++ src/main/java/org/sejda/sambox/pdmodel/font/PDType1Font.java
@@ -409,33 +409,41 @@ public class PDType1Font extends PDSimpleFont
             // this is important on systems with no installed fonts
             if (!encoding.contains(name))
             {
-                throw new IllegalArgumentException(
+                throw new UnsupportedCharacterException(
                         String.format("U+%04X ('%s') is not available in this font %s encoding: %s",
-                                unicode, name, getName(), encoding.getEncodingName()));
+                                unicode, name, getName(), encoding.getEncodingName()),
+                        unicode
+                );
             }
             if (".notdef".equals(name))
             {
-                throw new IllegalArgumentException(
-                        String.format("No glyph for U+%04X in font %s", unicode, getName()));
+                throw new UnsupportedCharacterException(
+                        String.format("No glyph for U+%04X in font %s", unicode, getName()),
+                        unicode
+                );
             }
         }
         else
         {
             if (!encoding.contains(name))
             {
-                throw new IllegalArgumentException(String.format(
+                throw new UnsupportedCharacterException(String.format(
                         "U+%04X ('%s') is not available in this font %s (generic: %s) encoding: %s",
                         unicode, name, getName(), genericFont.getName(),
-                        encoding.getEncodingName()));
+                        encoding.getEncodingName()),
+                        unicode
+                );
             }
 
             String nameInFont = getNameInFont(name);
 
             if (nameInFont.equals(".notdef") || !genericFont.hasGlyph(nameInFont))
             {
-                throw new IllegalArgumentException(
+                throw new UnsupportedCharacterException(
                         String.format("No glyph for U+%04X in font %s (generic: %s)", unicode,
-                                getName(), genericFont.getName()));
+                                getName(), genericFont.getName()),
+                        unicode
+                );
             }
         }
 
