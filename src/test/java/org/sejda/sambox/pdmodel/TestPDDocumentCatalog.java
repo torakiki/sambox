6a5b8d330371d699fd16e8c72ce9eaafcc91f061
Andrea Vacondio <andrea.vacondio@gmail.com>
2017-07-07 10:09:23 +0200

    close #76: porting of missing PDFBOX-3571

--- src/test/java/org/sejda/sambox/pdmodel/TestPDDocumentCatalog.java
+++ src/test/java/org/sejda/sambox/pdmodel/TestPDDocumentCatalog.java
@@ -77,7 +77,7 @@ public class TestPDDocumentCatalog
 
             // add an OutputIntent
             colorProfile = TestPDDocumentCatalog.class
-                    .getResourceAsStream("sRGB Color Space Profile.icm");
+                    .getResourceAsStream("sRGB.icc");
             // create output intent
             PDOutputIntent oi = new PDOutputIntent(doc, colorProfile);
             oi.setInfo("sRGB IEC61966-2.1");
