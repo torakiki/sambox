0840a8273059564749b95790fdeb2b1a1f8e7859
torakiki <andrea.vacondio@gmail.com>
2019-08-26 15:54:43 +0200

    fixed test failing on my machine for a rounding problem (not sure why it fails here and not on Travis)

--- src/test/java/org/sejda/sambox/pdmodel/interactive/form/PDTextFieldTest.java
+++ src/test/java/org/sejda/sambox/pdmodel/interactive/form/PDTextFieldTest.java
@@ -16,18 +16,20 @@
  */
 package org.sejda.sambox.pdmodel.interactive.form;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.sejda.sambox.cos.COSName;
 import org.sejda.sambox.pdmodel.PDDocument;
 import org.sejda.sambox.pdmodel.common.PDRectangle;
 import org.sejda.sambox.pdmodel.interactive.annotation.PDAnnotationWidget;
-
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.junit.Assert.*;
 
 /**
  * Test for the PDSignatureField class.
@@ -49,8 +51,9 @@ public class PDTextFieldTest
     public void createDefaultTextField()
     {
         PDField textField = new PDTextField(acroForm);
-        
-        assertEquals(textField.getFieldType(), textField.getCOSObject().getNameAsString(COSName.FT));
+
+        assertEquals(textField.getFieldType(),
+                textField.getCOSObject().getNameAsString(COSName.FT));
         assertEquals(textField.getFieldType(), "Tx");
     }
 
@@ -61,25 +64,27 @@ public class PDTextFieldTest
 
         assertNull(textField.getCOSObject().getItem(COSName.TYPE));
         assertNull(textField.getCOSObject().getNameAsString(COSName.SUBTYPE));
-        
+
         PDAnnotationWidget widget = textField.getWidgets().get(0);
-        
+
         assertEquals(COSName.ANNOT, textField.getCOSObject().getItem(COSName.TYPE));
         assertEquals(COSName.WIDGET.getName(),
                 textField.getCOSObject().getNameAsString(COSName.SUBTYPE));
-        
+
         assertEquals(widget.getCOSObject(), textField.getCOSObject());
     }
 
     @Test
-    public void textValueNotCropped_Width() throws IOException {
+    public void textValueNotCropped_Width() throws IOException
+    {
         PDTextField textField = new PDTextField(acroForm);
         PDAnnotationWidget widget = textField.getWidgets().get(0);
         widget.setRectangle(new PDRectangle(0, 0, 100, 20));
         textField.setValue("This is a long text field value that could get cropped");
 
-        byte[] bytes = widget.getAppearance().getNormalAppearance().getAppearanceStream().getContentStream().toByteArray();
+        byte[] bytes = widget.getAppearance().getNormalAppearance().getAppearanceStream()
+                .getContentStream().toByteArray();
         String appearanceString = new String(bytes, StandardCharsets.UTF_8);
-        assertThat(appearanceString, containsString("/Helvetica 4.2228 Tf"));
+        assertThat(appearanceString, containsString("/Helvetica 4.222"));
     }
 }
